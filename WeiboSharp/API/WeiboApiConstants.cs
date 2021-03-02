69ae8ac295fb7b7574e57b67c74d2ef721d3ef70
Timothy Makkison <timmakkison@gmail.com>
2021-03-02 00:33:57 +0000

    Edit Urls to be concated

--- WeiboSharp/API/WeiboApiConstants.cs
+++ WeiboSharp/API/WeiboApiConstants.cs
@@ -6,15 +6,21 @@ namespace WeiboSharp.API
 {
     internal  static class WeiboApiConstants
     {
+        
         public const string WEIBO_URL = "http://api.t.sina.com.cn/";
+        public const string BASE_M_URL = "https://m.weibo.cn/api";
 
+        public const string CONTAINER = "/container";
+        public const string STATUS = "/statuses";
         // Container
-        public const string CONTAINER_GET_USER = "https://m.weibo.cn/api/container/getIndex?containerid=100505{0}";
-        public const string CONTAINER_GET_USER_INFO = "https://m.weibo.cn/api/container/getIndex?containerid=230283{0}_-_INFO";
-        public const string CONTAINER_GET_USER_PAGE = "https://m.weibo.cn/api/container/getIndex?containerid=107603{0}&page={1}";
+        public const string CONTAINER_GET_USER = BASE_M_URL + CONTAINER + "/getIndex?containerid=100505{0}";
+        public const string CONTAINER_GET_USER_INFO = BASE_M_URL + CONTAINER + "/getIndex?containerid=230283{0}_-_INFO";
+        public const string CONTAINER_GET_USER_PAGE = BASE_M_URL + CONTAINER + "/getIndex?containerid=107603{0}&page={1}";
 
         //Statuses
-        public const string STATUS_GET_CONTENT = "https://m.weibo.cn/statuses/show?id={0}";
+        public const string STATUS_GET_POST = "https://m.weibo.cn/statuses/show?id={0}";
+        public const string STATUS_GET_EXTENDED_POST = BASE_M_URL + STATUS + "/extend?id={0}";
+
 
         // Timeline
 
